dd89a2a0c2e5ea6293772117d08a3121f51487cd
hiagopro <areazica@gmail.com>
2025-03-31 11:40:31 -0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
   return <div></div>;
 }
